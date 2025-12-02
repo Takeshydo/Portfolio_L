d0364feca696734287d8630cfbb6f8b4b82cbe32
MathysL <m.lacoque@ecole-ippsi.net>
2025-12-02 14:01:12 +0100

    conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     output: 'static',
     site: 'https://takeshydo.github.io/Portfolio_L',
-    base: '/Portfolio/',
+    base: '/Portfolio_L/',
     prefetch: true
 });
